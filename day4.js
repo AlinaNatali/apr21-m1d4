9ebd0255d88252330b3e5e0abb1a7b466a37df5d
AlinaNatali <82570010+AlinaNatali@users.noreply.github.com>
2021-04-15 08:55:00 +0100

    new instruc

--- day4.js
+++ day4.js
@@ -1,7 +1,12 @@
+
+
+// create a profile on github
+// create a repository every day
+// clone that repository in local folder on your computer (file - > clone repository)
+// open that folder with vscode and have fun!
+
 // steps for pushing local modifications into our online github
-
 // 1) add the files to the staging area (a place for terporarily store our modifications) git add
-
 // 2) create a commit out of the files in the area 
 // a commit is a bundle of some modification
 // hit commit
@@ -9,3 +14,4 @@
 // git push
 
 let strive = 5 
+
