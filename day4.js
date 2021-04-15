8ba641acf01f11c5d9ab8e76a24f10a88ae9b611
AlinaNatali <82570010+AlinaNatali@users.noreply.github.com>
2021-04-15 08:42:29 +0100

    adds day4 js file

---
+++ day4.js
@@ -0,0 +1,9 @@
+// steps for pushing local modifications into our online github
+
+// 1) add the files to the staging area (a place for terporarily store our modifications) git add
+
+// 2) create a commit out of the files in the area 
+// a commit is a bundle of some modification
+// hit commit
+// 3) we need to push our commit online 
+// git push
